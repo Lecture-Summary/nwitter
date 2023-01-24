d59e25e3e97ade8eecf81a6314b2445883a2cff2
HoJin9622 <kiss0104040@gmail.com>
2023-01-24 19:39:39 +0900

    #1.0 React + Firebase Setup

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBpNujTC6FbhAz8Q30ClHMvMLgc6hmTLM',
+  authDomain: 'nwitter-f422b.firebaseapp.com',
+  projectId: 'nwitter-f422b',
+  storageBucket: 'nwitter-f422b.appspot.com',
+  messagingSenderId: '802782105500',
+  appId: '1:802782105500:web:57609448ca498307460259',
+}
+
+const app = initializeApp(firebaseConfig)
+
+export default app
