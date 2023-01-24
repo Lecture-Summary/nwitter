3df60d86cf0ecf8447d5aa03ce52167aa016d86e
HoJin9622 <kiss0104040@gmail.com>
2023-01-24 23:27:44 +0900

    #3.1 Nweeting!

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,38 @@
-const Home = () => <span>Home</span>
+import { dbService } from 'fbase'
+import { addDoc, collection } from 'firebase/firestore'
+import { useState } from 'react'
+
+const Home = () => {
+  const [nweet, setNweet] = useState('')
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    await addDoc(collection(dbService, 'nweets'), {
+      nweet,
+      createdAt: Date.now(),
+    })
+    setNweet('')
+  }
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event
+    setNweet(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type='text'
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type='submit' value='Nweet' />
+      </form>
+    </div>
+  )
+}
 
 export default Home
