484896609e11b7e0b49a68dd63a56b94087b4562
HoJin9622 <kiss0104040@gmail.com>
2023-01-28 22:28:34 +0900

    #3.5 Delete and Update part Two

--- src/components/NweetItem.tsx
+++ src/components/NweetItem.tsx
@@ -1,3 +1,6 @@
+import { dbService } from 'fbase'
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore'
+import { useState } from 'react'
 import { Nweet } from 'types/nweet'
 
 interface Props {
@@ -6,13 +9,52 @@ interface Props {
 }
 
 const NweetItem = ({ nweetObj, isOwner }: Props) => {
+  const [editing, setEditing] = useState(false)
+  const [newNweet, setNewNweet] = useState(nweetObj.text)
+  const onDeleteClick = () => {
+    const ok = window.confirm('Are you sure you want to delete this nweet?')
+    if (ok) {
+      deleteDoc(doc(dbService, 'nweets', nweetObj.id))
+    }
+  }
+  const toggleEditing = () => setEditing((prev) => !prev)
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    updateDoc(doc(dbService, 'nweets', nweetObj.id), { text: newNweet })
+    setEditing(false)
+  }
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event
+    setNewNweet(value)
+  }
+
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type='text'
+              placeholder='Edit your nweet'
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type='submit' value='Update Nweet' />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
         </>
       )}
     </div>
